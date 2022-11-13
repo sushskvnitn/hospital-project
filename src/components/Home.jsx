28badaf0bbe0acef5021a726c223d93105feb41d
Sushant Kogurwar <Sushantkogurwar@gmail.com>
2022-11-14 00:07:12 +0530

    added box in updates

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
       <iframe src="https://embed.lottiefiles.com/animation/22463" className="text-center hospital_animation "  title="hi"  style={{ pointerEvents: "none" }}></iframe>
     </div>
     <div style={{marginTop:"35vh" ,zIndex:"1" }}>
-      <h1 className="text-center ">Narayana Hospital</h1>
+      <h1 className="text-center " style={{ color: "#043d78" }}>Narayana Hospital</h1>
       <p className="fs-5 text-center"> location : Lorem ipsum dolor sit amet consectetur adipisicing elit. Id in porro recusandae? </p>
     </div>
       
--- src/components/updates/updates.jsx
+++ src/components/updates/updates.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const updates = () => {
   return (
     <>
-      
+      <h3 class="mb-4 text-center m-3 py-3" id="updates">
+        Updates
+      </h3>
+      <div className="d-flex justify-content-center">
+        <div className="box"></div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default updates
+export default updates;
