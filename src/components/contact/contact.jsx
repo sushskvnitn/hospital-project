4fb8abc01b1815058c9cf2b726828faa1c709f14
salilganu <sarc.rheumatology@gmail.com>
2023-01-05 14:42:43 +0530

    map change

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -7,7 +7,7 @@ const contact = () => {
       <h1 className ="text-center"  > <b> Contact us </b> </h1>
       <div className="d-flex justify-content-center" >
         <iframe title="map" className="map d-lg-none" src="https://goo.gl/maps/QPMwkt9U7fHiV9Qz5" width="390" height="400" style={{border:"0"}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
-        <iframe title="map" className="map d-none d-lg-block" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3721.4256400999966!2d79.06903!3d21.1354529!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bd4c0893c950247%3A0x73188ded64c1f355!2sPeople&#39;s%20Gym!5e0!3m2!1sen!2sin!4v1672067129596!5m2!1sen!2sin" width="1500" height="500" style={{border:"0"}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+        <iframe title="map" className="map d-none d-lg-block" src="https://goo.gl/maps/QPMwkt9U7fHiV9Qz5" width="1500" height="500" style={{border:"0"}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
       </div>
   <div className="container">
     <div className="row contact-container">
